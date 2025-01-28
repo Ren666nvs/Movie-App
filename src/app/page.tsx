2848fd14f60eccd0dff4576104c393474cff76e0
Ren666nvs <164177806+Ren666nvs@users.noreply.github.com>
2025-01-28 22:48:48 +0800

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import { Button } from "@/components/ui/button";
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectValue } from "@/components/ui/select";
 import { SelectTrigger } from "@radix-ui/react-select";
+import { headers } from "next/headers";
+
 export default function Home() {
   return (
     <div className="...">
       <Button>hello button</Button>
       <Select>
       <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder="Select a fruit" />
+        <SelectValue placeholder="Select a fruit" /> 
       </SelectTrigger>
       <SelectContent>
         <SelectGroup>
@@ -19,7 +21,30 @@ export default function Home() {
           <SelectItem value="pineapple">Pineapple</SelectItem>
         </SelectGroup>
       </SelectContent>
-    </Select>
+     </Select>
+    
       </div>
       )
+   }
+   const TMDB_BASE_URL = process.env.TMDB_BASE_URL;
+   const TMDB_API_TOKEN = process.env.TMDB_API_TOKEN;
+   const getMovieData = async () => {
+    try{
+      console.log("working");
+      
+      const response = axios.get(
+        `${TMDB_BASE_URL}/movie/popular?language=en-US&page=1`, 
+        {headers: {
+      Authorization: `Bearer ${TMDB_API_TOKEN}` ,
+      },
+      }
+    );
+    console.log(response);
+    
+    }
+    catch(err){
+      console.log(err);
+      
+    }
+  
    }
